c397602e08d775f7120abbeef4fdb6291fbdb168
Johan Larsson <johanlarsson@outlook.com>
2023-12-06 20:28:22 +0100

    chore: remove FindSphinx cmake function

--- cmake/FindSphinx.cmake
+++
@@ -1,8 +0,0 @@
-include(FindPackageHandleStandardArgs)
-
-find_program(
-    SPHINX_EXECUTABLE
-    NAMES sphinx-build sphinx-build.exe)
-mark_as_advanced(SPHINX_EXECUTABLE)
-
-find_package_handle_standard_args(Sphinx DEFAULT_MSG SPHINX_EXECUTABLE)
