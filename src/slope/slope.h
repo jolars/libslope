a51b80b4c49f2b74ac3469f6ef7ca6396836fba5
Johan Larsson <johanlarsson@outlook.com>
2023-12-06 20:17:52 +0100

    docs: add file level documentation and build groups for slope.h

--- src/slope/slope.h
+++ src/slope/slope.h
@@ -1,8 +1,18 @@
+/**
+ * @file
+ * @brief This file contains the declaration of the slope namespace and its
+ * functions for calculating slope coefficients in linear regression models.
+ */
+
 #include "parameters.h"
 #include "results.h"
 #include <Eigen/Sparse>
 
 namespace slope {
+
+/** @defgroup estimators Estimators
+ *  This is the first group
+ */
 
 /**
  * Calculates the slope coefficients for a linear regression model using the
@@ -17,6 +27,7 @@ namespace slope {
  *   regularization. If not provided, it will be set to zero.
  * @return The slope coefficients, intercept values, and primal values for each
  *   step in the regularization path.
+ * @ingroup estimators
  */
 Results
 slope(const Eigen::MatrixXd& x,
@@ -29,7 +40,7 @@ slope(const Eigen::MatrixXd& x,
  * Calculates the slope coefficients for a linear regression model using the
  * SortedL1Norm regularization.
  *
- * @param x The sparse input matrix of size n x p, where n is the number of
+ * @param x Sparse input matrix of size n x p, where n is the number of
  *   observations and p is the number of predictors.
  * @param y The response matrix of size n x 1.
  * @param alpha The regularization parameter sequence. If not provided, it will
@@ -38,6 +49,7 @@ slope(const Eigen::MatrixXd& x,
  *   regularization. If not provided, it will be set to zero.
  * @return The slope coefficients, intercept values, and primal values for each
  *   step in the regularization path.
+ * @ingroup estimators
  */
 Results
 slope(const Eigen::SparseMatrix<double>& x,
@@ -45,5 +57,7 @@ slope(const Eigen::SparseMatrix<double>& x,
       Eigen::ArrayXd alpha = Eigen::ArrayXd::Zero(0),
       Eigen::ArrayXd lambda = Eigen::ArrayXd::Zero(0),
       const SlopeParameters& params = SlopeParameters());
+
+/** @} */ // end of group1
 
 } // namespace slope
