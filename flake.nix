d1c324c2a2bb6abeebbbb2a62f7544f08f3d588b
Johan Larsson <jolars@posteo.com>
2025-01-28 08:11:05 +0100

    chore: switch to use flake for development

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bashInteractive
+            catch2_3
+            clang
+            clang-tools
+            cmake
+            doxygen
+            eigen
+            gcc
+            gdb
+            lldb
+          ];
+        };
+      }
+    );
+}
