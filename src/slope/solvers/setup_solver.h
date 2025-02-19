2731cf0dd06f4c567dda5c3e5b9c1fc5eacd0ccb
Johan Larsson <jolars@posteo.com>
2025-02-19 14:55:41 +0100

    docs: add documentation for `setupSolver()`

--- src/slope/solvers/setup_solver.h
+++ src/slope/solvers/setup_solver.h
@@ -4,6 +4,29 @@
 
 namespace slope {
 
+/**
+ * @brief Factory function to create and configure a SLOPE solver
+ *
+ * @details Creates a solver object based on the specified type and parameters.
+ * The solver implements the Sorted L1 Penalized Estimation (SLOPE) algorithm
+ * with various configurations possible.
+ *
+ * @param solver_type Type of solver to use (e.g., "pgd", "admm")
+ * @param objective Type of objective function ("gaussian", "binomial",
+ * "poisson", "multinomial")
+ * @param tol Convergence tolerance for the solver
+ * @param max_it_inner Maximum number of inner iterations
+ * @param standardize_jit Whether to standardize features just-in-time
+ * @param print_level Level of verbosity for solver output (0: none, 1: some, 2:
+ * verbose)
+ * @param intercept Whether to fit an intercept term
+ * @param update_clusters Whether to update cluster assignments during
+ * optimization
+ * @param pgd_freq Frequency of proximal gradient descent updates
+ *
+ * @return std::unique_ptr<solvers::SolverBase> A unique pointer to the
+ * configured solver
+ */
 std::unique_ptr<solvers::SolverBase>
 setupSolver(const std::string& solver_type,
             const std::string& objective,
