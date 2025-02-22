22280e058413c069658c209295ffea570629b194
Johan Larsson <jolars@posteo.com>
2025-02-22 11:20:19 +0100

    chore: remove unused header include

--- tests/normalization.cpp
+++ tests/normalization.cpp
@@ -6,7 +6,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
-#include <limits>
 
 std::tuple<Eigen::VectorXd, Eigen::VectorXd>
 computeMeanAndStdDev(const Eigen::MatrixXd& x)
