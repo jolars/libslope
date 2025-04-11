6e306a78db35139e6fed279c705585cbf4adc3e5
Johan Larsson <jolars@posteo.com>
2025-04-11 14:02:40 +0200

    fix: add missing threads header

--- tests/math.cpp
+++ tests/math.cpp
@@ -2,6 +2,7 @@
 #include "generate_data.hpp"
 #include "slope/normalize.h"
 #include "test_helpers.hpp"
+#include <slope/threads.h>
 #include <Eigen/Core>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers.hpp>
